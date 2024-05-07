61e15f7130fc09dab4b54e4daf2a3cc5421cacae
unknown <eduardo.soares.dev0@gmail.com>
2024-05-07 15:22:46 -0300

    configurando docker e ajustes front

--- frontend/src/components/LastProject/index.jsx
+++ frontend/src/components/LastProject/index.jsx
@@ -22,46 +22,57 @@ export default function LastProject() {
   }, []);
 
   return (
-    <section
-      data-aos="fade-up"
-      className="flex flex-col items-center justify-center min-h-screen bg-lastProject-vetor bg-no-repeat bg-center px-5 md:px-10 mb-20 md:mb-0 mt-20 md:mt-0"
-    >
-      <div data-aos="fade-up">
-        <TituloSecundario>Ultimo projeto</TituloSecundario>
-      </div>
+    <>
       {projetos[projetos.length - 1] && (
-        <div
-          className="flex flex-col md:flex-row flex-nowrap items-center mt-10 w-full md:w-7/12 "
+        <section
           data-aos="fade-up"
+          className="flex flex-col items-center justify-center min-h-screen bg-lastProject-vetor bg-no-repeat bg-center px-5 md:px-10 mb-20 md:mb-0"
         >
-          <div className="w-full md:w-2/5 flex flex-col justify-between items-start order-2 md:order-1">
-            <p className="text-2xl pl-4 border-l-2 border-white mb-6">
-              {projetos[projetos.length - 1].titulo}
-            </p>
-            <div>
-              <p className="mb-4">
-              {(projetos[projetos.length - 1].descricao).substring(0,200) + "..."}
-              </p>
-              <ul className="flex flex-wrap justify-end md:justify-start mb-6">
-                {projetos[projetos.length - 1].skills.map((skill, i) => {
-                  if(i <= 2){
-                    return (
-                      <SkillsContainer key={skill.titulo} skill={skill}></SkillsContainer>
-                    )
-                  }
-                })}
-              </ul>
-            </div>
-            <Link
-              to={"/projetos"}
-              className="border-white border-2 px-4 py-1 rounded-md"
-            >
-              ver mais
-            </Link>
+          <div data-aos="fade-up">
+            <TituloSecundario>Ultimo projeto</TituloSecundario>
           </div>
-          <img src={`${import.meta.env.VITE_API_URL}/imagem/${projetos[projetos.length - 1].imagem}`} className="inline-block w-full mb-6 md:mb-0 md:w-3/5 h-48 mt-10 sm:mt-0 sm:h-72 bg-white md:ml-10 order-1 md:order-2"></img>
-        </div>
+          <div
+            className="flex flex-col lg:flex-row flex-nowrap items-center mt-10 w-full lg:w-7/12 "
+            data-aos="fade-up"
+          >
+            <div className="w-full md:w-3/5 flex flex-col justify-between items-start order-2 lg:order-1 mb-10 lg:mb-0">
+              <p className="text-2xl pl-4 border-l-2 border-white mb-6">
+                {projetos[projetos.length - 1].titulo}
+              </p>
+              <div>
+                <p className="mb-4">
+                  {projetos[projetos.length - 1].descricao.substring(0, 200) +
+                    "..."}
+                </p>
+                <ul className="flex flex-wrap justify-end lg:justify-start mb-6">
+                  {projetos[projetos.length - 1].skills.map((skill, i) => {
+                    if (i <= 2) {
+                      return (
+                        <SkillsContainer
+                          key={skill.titulo}
+                          skill={skill}
+                        ></SkillsContainer>
+                      );
+                    }
+                  })}
+                </ul>
+              </div>
+              <Link
+                to={"/projetos"}
+                className="border-white border-2 px-4 py-1 rounded-md"
+              >
+                ver mais
+              </Link>
+            </div>
+            <img
+              src={`${import.meta.env.VITE_API_URL}/imagem/${
+                projetos[projetos.length - 1].imagem
+              }`}
+              className="inline-block w-full mb-6 md:mb-0 md:w-3/5 mt-10 sm:mt-0 bg-white lg:ml-10 order-1 md:order-2"
+            ></img>
+          </div>
+        </section>
       )}
-    </section>
+    </>
   );
 }
--- frontend/src/components/SobreMim/index.jsx
+++ frontend/src/components/SobreMim/index.jsx
@@ -1,65 +1,105 @@
 import { Link } from "react-router-dom";
-import me from "../../assets/img/me.png"
+import me from "../../assets/img/me.png";
 import TituloSecundario from "../TituloSecundario";
 import SkillsContainer from "../SkillsContainer";
 import { useEffect, useState } from "react";
 import { buscaSkills } from "../../services/skills";
 
 export default function SobreMim() {
-  const [skills, setSkills] = useState()
+  const [skills, setSkills] = useState();
 
   const topicos = [
-    ["Desenvolvedor Full-Stack Apaixonado","Com anos de estudo e prática autodidata."],
-    ["Habilidades em Tecnologias Avançadas","Experiência em linguagens de programação para front-end e back-end, e metodologias ágeis."],
-    ["Colaboração e Comunicação","Valorizo o trabalho em equipe e a comunicação eficaz."],
-    ["Adaptabilidade","Capacidade de se adaptar rapidamente a novos desafios."],
-    ["Busca por Oportunidades","Desejo de contribuir com paixão e crescimento contínuo em tecnologia."],
-    ["Entusiasmo","Pronto para se juntar a uma equipe dinâmica e inovadora e alinhar habilidades com as suas necessidades."],
-  ]
+    ["Desenvolvedor Full-Stack", "Com anos de estudo e prática autodidata."],
+    [
+      "Habilidades em Tecnologias Avançadas",
+      "Experiência em linguagens de programação para front-end e back-end, e metodologias ágeis.",
+    ],
+    [
+      "Colaboração e Comunicação",
+      "Valorizo o trabalho em equipe e a comunicação eficaz.",
+    ],
+    [
+      "Adaptabilidade",
+      "Capacidade de se adaptar rapidamente a novos desafios.",
+    ],
+  ];
 
   useEffect(() => {
-      const carregaSkills = async () => {
-        try {
-          const skillsCarregadas = await buscaSkills();
-          setSkills(skillsCarregadas);
-        } catch (error) {
-          // Lidar com erros
-          console.error('Erro ao carregar as habilidades:', error);
-        }
-      };
-  
-      carregaSkills();
-  },[])
+    const carregaSkills = async () => {
+      try {
+        const skillsCarregadas = await buscaSkills();
+        setSkills(skillsCarregadas);
+      } catch (error) {
+        // Lidar com erros
+        console.error("Erro ao carregar as habilidades:", error);
+      }
+    };
 
-    return(
-        <section data-aos="fade-up" className="flex items-center flex-col md:flex-row min-h-svh justify-center relative bg-sobreMim-vetor bg-center bg-no-repeat px-10 mt-20 md:mt-10 ">
-        <img className="w-full md:w-1/4 md:mr-6 mb-10 md:mb-0" data-aos="fade-up" data-aos-delay="50" src={me}></img>
-        <div className="md:ml-10 w-4/5 md:w-2/5 text-center md:text-left" data-aos="fade-up" data-aos-delay="50">
+    carregaSkills();
+  }, []);
+
+  return (
+    <section
+      data-aos="fade-up"
+      className="flex items-center flex-col min-h-svh justify-center relative bg-sobreMim-vetor bg-center bg-no-repeat px-10 my-32 lg:my-0 "
+    >
+      <div data-aos="fade-up" className="flex flex-col lg:flex-row justify-center items-center sm:px-24 lg:px-0">
+        <img
+          className="w-full lg:w-1/4 mb-10 mr-0 lg:mr-6 lg:mb-0"
+          data-aos="fade-up"
+          data-aos-delay="50"
+          src={me}
+        ></img>
+        <div
+          className="md:ml-10 text-center md:text-left max-w-2xl"
+          data-aos="fade-up"
+          data-aos-delay="50"
+        >
           <TituloSecundario>Olá, Meu nome é Eduardo Soares</TituloSecundario>
-          <ul className="mb-6 list-disc">
-            {topicos.map((topico, i) => <li className="mb-3"><strong>{topico[0]}: </strong>{topico[1]}</li>)}
-          </ul>
-          <ul className="flex items-center mb-6 flex-wrap">
-            <p className="font-bold mr-3">skills:</p>
-            {skills && skills.map(skill => (
-              <SkillsContainer key={skill.titulo} skill={skill}></SkillsContainer>
+          <ul className="mb-6 list-disc px-7 sm:px-0">
+            {topicos.map((topico, i) => (
+              <li className="mb-3">
+                <strong>{topico[0]}: </strong>
+                {topico[1]}
+              </li>
             ))}
           </ul>
-          <div className="flex">
-            <p className="font-bold mr-4">Fale comigo:</p>
-            <ul className="flex items-center">
-              <li>
-                <Link to={""} className="w-7 h-7 bg-linkedin bg-center bg-cover mr-4 inline-block"></Link>
-              </li>
-              <li>
-                <Link className="w-7 h-7 bg-github-icon bg-center bg-cover mr-4 inline-block"></Link>
-              </li>
-              <li>
-                <Link to={"https://api.whatsapp.com/send?phone=+554388742099&text=Olá%20gostaria%20de%20fazer%20um%20orçamento"} className="w-7 h-7 bg-whatsapp-icon bg-center bg-cover mr-4 inline-block"></Link>
-              </li>
-            </ul>
-          </div>
         </div>
-      </section>
-    )
+      </div>
+      <ul data-aos="fade-up" className="flex items-center mb-6 flex-wrap">
+        <p className="font-bold mr-3">skills:</p>
+        {skills &&
+          skills.map((skill) => (
+            <SkillsContainer key={skill.titulo} skill={skill}></SkillsContainer>
+          ))}
+      </ul>
+      <div data-aos="fade-up" className="flex flex-col items-center text-center">
+        <div className="flex">
+          <p className="font-bold mr-4">Fale comigo:</p>
+          <ul className="flex items-center">
+            <li>
+              <Link
+                to={"https://linkedin.com/in/eduardo-soares-14ab94233"}
+                className="w-7 h-7 bg-linkedin bg-center bg-cover mr-4 inline-block hover:scale-125"
+              ></Link>
+            </li>
+            <li>
+              <Link to={"https://github.com/deexty"} className="w-7 h-7 bg-github-icon bg-center bg-cover mr-4 inline-block hover:scale-125"></Link>
+            </li>
+            <li>
+              <Link
+                to={
+                  "https://api.whatsapp.com/send?phone=+554388742099&text=Olá%20gostaria%20de%20fazer%20um%20orçamento"
+                }
+                className="w-7 h-7 bg-whatsapp-icon bg-center bg-cover mr-4 inline-block hover:scale-125"
+              ></Link>
+            </li>
+          </ul>
+        </div>
+        <p className="text-xs italic mt-2 opacity-75">
+          desenvolvimento profissional e educacional presentes no linkedin
+        </p>
+      </div>
+    </section>
+  );
 }
